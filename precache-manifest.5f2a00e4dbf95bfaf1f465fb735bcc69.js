53a685f1673e28c1ee89263c385be717e3baca2f
DrCardamom <erkshimba@gmail.com>
2020-05-04 13:51:06 -0400

    Updates

---
+++ precache-manifest.5f2a00e4dbf95bfaf1f465fb735bcc69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc83939db364cc4ee6f49b88b72dcb80",
+    "url": "/RoboRedux/index.html"
+  },
+  {
+    "revision": "665fe56f0ec56c163489",
+    "url": "/RoboRedux/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a84460781eebb9ac669d",
+    "url": "/RoboRedux/static/css/main.4a21d5bc.chunk.css"
+  },
+  {
+    "revision": "665fe56f0ec56c163489",
+    "url": "/RoboRedux/static/js/2.82cf09cf.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/RoboRedux/static/js/2.82cf09cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84460781eebb9ac669d",
+    "url": "/RoboRedux/static/js/main.f684b6f4.chunk.js"
+  },
+  {
+    "revision": "ec3ff17b8f7467978513",
+    "url": "/RoboRedux/static/js/runtime-main.0732b1ea.js"
+  }
+]);
