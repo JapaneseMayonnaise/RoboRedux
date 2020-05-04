90906d1440089fafd04d12e13db47c764a69a19f
DrCardamom <erkshimba@gmail.com>
2020-05-04 12:17:05 -0400

    add another reducer

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import { searchRobots, requestRobots } from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
 import App from './containers/App';
-import { searchRobots, requestRobots } from './reducer';
-
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+   
 const logger = createLogger();
-const rootReducer = combineReducers({searchRobots, requestRobots});
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+const rootReducer =  combineReducers({searchRobots, requestRobots});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducer, 
+                          applyMiddleware(thunkMiddleware, logger));  
 
+// Provider passes store all the way down to the bottom hierarchy 
+// of the component tree
+ReactDOM.render(<Provider store={store}>
+                  <App />
+                </Provider>, document.getElementById('root'));
+
+registerServiceWorker();
--- src/reducer.js
+++ src/reducer.js
@@ -1,42 +1,37 @@
-import { 
-    CHANGE_SEARCH_FIELD,
+import { CHANGE_SEARCH_FIELD,
     REQUEST_ROBOTS_PENDING,
     REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAIL,
+    REQUEST_ROBOTS_FAILED
  } from './constants.js';
-
-const initialStateSearch = {
+ 
+ const initialState_searchRobots = {
     searchField: ''
-}
-
-export const searchRobots = (state=initialStateSearch, action={}) => {    
+ }
+ 
+ export const searchRobots = (state=initialState_searchRobots, action={}) => {
     switch(action.type) {
-        case CHANGE_SEARCH_FIELD:
-                return Object.assign({}, state, {searchField: action.payload});
-
-        default:
-                return state;
+       case CHANGE_SEARCH_FIELD: 
+          return Object.assign({}, state, {searchField: action.payload})
+       default: 
+          return state;
     }
-}
-
-const initialStateRobots = {
+ }
+ 
+ const initialState_requestRobots = {
     isPending: false,
     robots: [],
     error: ''
-}
-
-export const requestRobots = (state=initialStateRobots, action={}) => {
+ }
+ 
+ export const requestRobots = (state=initialState_requestRobots, action={}) => {
     switch(action.type) {
-        case REQUEST_ROBOTS_PENDING:
-            return Object.assign({}, state, {isPending: true});
-        case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}, state, {robots: action.payload, isPending: false});
-        case REQUEST_ROBOTS_FAIL:
-            return Object.assign({}, state, {error: action.payload, isPending: false});
-        default:
-            return state;
+       case REQUEST_ROBOTS_PENDING:
+          return Object.assign({}, state, {isPending: true})
+       case REQUEST_ROBOTS_SUCCESS:
+          return Object.assign({}, state, {isPending: false, robots: action.payload})
+       case REQUEST_ROBOTS_FAILED:
+          return Object.assign({}, state, {isPending: false, error: action.payload})
+       default:
+          return state;
     }
-}
-
-// WHAT REDUCERS DO 
-// takes care of states 
+ }
